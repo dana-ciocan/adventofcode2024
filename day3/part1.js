1fff87a449b0db062113c37ddcf682957c1db671
Dana Ciocan <danaciocan@gmail.com>
2024-12-08 21:18:23 +0000

    Complete day 3 part 1

---
+++ day3/part1.js
@@ -0,0 +1,10 @@
+import { program } from "./input.js";
+
+const regexp = /mul\([0-9]*,[0-9]*\)/g;
+const matches = [...program.matchAll(regexp)];
+
+console.log(matches.reduce((acc, [val]) => {
+    const numbers = val.replace('mul(', '').replace(')', '').split(',').map(num => parseInt(num, 10));
+    return acc + numbers[0] * numbers[1];
+}, 0));
+
