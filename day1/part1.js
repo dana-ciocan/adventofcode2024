259a92d20d714cf98139a53db4acbfda00de3baf
Dana Ciocan <dana@Danas-MacBook-Pro.local>
2024-12-08 15:04:46 +0000

    Complete day 1, part 1

---
+++ day1/part1.js
@@ -0,0 +1,11 @@
+import { list1, list2 } from './part1_input.js';
+
+const sortedList1 = list1.sort();
+const sortedList2 = list2.sort();
+
+const differences = sortedList1.reduce((acc, val, index) => {
+    const difference = val - sortedList2[index];
+    return acc + (difference > 0 ? difference : -difference);
+}, 0)
+
+console.log('differences: ', differences);
