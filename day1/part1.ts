5042c035fc9dd97b91ba715c7efb705af5a563af
Dana Ciocan <danaciocan@gmail.com>
2024-12-09 19:13:57 +0000

    fixup! Convert day 1 to TS

--- day1/part1.ts
+++ day1/part1.ts
@@ -3,7 +3,7 @@ import { list1, list2 } from './input.ts';
 const sortedList1 = list1.sort();
 const sortedList2 = list2.sort();
 
-const differences = sortedList1.reduce((acc, val, index) => {
+const differences = sortedList1.reduce((acc: number, val: number, index: number) => {
     const difference = val - sortedList2[index];
     return acc + (difference > 0 ? difference : -difference);
 }, 0)
--- day1/part2.ts
+++ day1/part2.ts
@@ -1,6 +1,6 @@
 import { list1, list2 } from './input.ts';
 
-const similarityScore = list1.reduce((acc, val) => {
+const similarityScore = list1.reduce((acc: number, val: number) => {
     const numTimesInList2 = list2.filter(locationId => locationId === val).length;
     return acc + (val * numTimesInList2);
 }, 0)
