6b15fe7bb26df744404f68a539b3e7de3f0bcf2a
Dana Ciocan <danaciocan@gmail.com>
2024-12-14 16:44:07 +0000

    Add isGuardOutOfBounds()

---
+++ day6/is-guard-out-of-bounds.test.ts
@@ -0,0 +1,37 @@
+import { expect, test } from 'vitest';
+import { isGuardOutOfBounds } from './is-guard-out-of-bounds';
+
+const testMap = ['....#.....',
+  '.........#',
+  '..........',
+  '..#.......',
+  '.......#..',
+  '..........',
+  '.#..^.....',
+  '........#.',
+  '#.........',
+  '......#...'];
+
+test('should return false if guard is inside the map', () => {
+  expect(isGuardOutOfBounds([2, 2], testMap)).toEqual(false);
+})
+
+test('should return false if guard is in the top left corner', () => {
+  expect(isGuardOutOfBounds([0, 0], testMap)).toEqual(false);
+})
+
+test('should return false if guard is in the top right corner', () => {
+  expect(isGuardOutOfBounds([0, 9], testMap)).toEqual(false);
+})
+
+test('should return false if guard is in the bottom right corner', () => {
+  expect(isGuardOutOfBounds([9, 9], testMap)).toEqual(false);
+})
+
+test('should return false if guard is in the bottom left corner', () => {
+  expect(isGuardOutOfBounds([9, 0], testMap)).toEqual(false);
+})
+
+test('should return true if guard is outside the map', () => {
+  expect(isGuardOutOfBounds([-1, -1], testMap)).toEqual(true);
+})
---
+++ day6/is-guard-out-of-bounds.ts
@@ -0,0 +1,8 @@
+export const isGuardOutOfBounds = (currentGuardPosition: number[], map: string[]) => {
+  const [guardRow, guardCol] = currentGuardPosition;
+
+  if (guardRow >= 0 && guardCol >= 0 && guardRow < map.length && map[guardRow]?.[guardCol]) {
+    return false;
+  }
+  return true;
+};
