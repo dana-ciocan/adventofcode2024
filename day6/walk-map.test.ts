62a665ef73f6e4ce350e4fb7c674277f00783262
Dana Ciocan <danaciocan@gmail.com>
2024-12-14 19:17:51 +0000

    Remove extra copy of map

--- day6/walk-map.test.ts
+++ day6/walk-map.test.ts
@@ -15,14 +15,3 @@ const testMap = ['....#.....',
 test('should return the right answer for the test map', () => {
   expect(walkMap(testMap)).toBe(41);
 });
-
-'....#.....',
-  '....XXXXX#',
-  '....X...X.',
-  '..#.X...X.',
-  '..XXXXX#X.',
-  '..X.X.X.X..',
-  '.#XXXXXXX.',
-  '.XXXXXXX#.',
-  '#XXXXXXX..',
-  '......#X..'
