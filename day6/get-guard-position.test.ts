9f41756d9133833c62863ebd709905d009bc446e
Dana Ciocan <danaciocan@gmail.com>
2024-12-14 14:44:13 +0000

    Add getGuardPosition

---
+++ day6/get-guard-position.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from 'vitest';
+import { getGuardPosition } from './get-guard-position';
+
+test('should find the guard in one row', () => {
+  expect(getGuardPosition(['.#..^.....'])).toEqual([0, 4]);
+})
+
+test('should find the guard in a full map', () => {
+  expect(getGuardPosition(['....#.....',
+    '.........#',
+    '..........',
+    '..#.......',
+    '.......#..',
+    '..........',
+    '.#..^.....',
+    '........#.',
+    '#.........',
+    '......#...'])).toEqual([6, 4]);
+})
---
+++ day6/get-guard-position.ts
@@ -0,0 +1,12 @@
+export const getGuardPosition = (map) => {
+  let guardRow;
+  let guardCol;
+  for (let i = 0; i < map.length; i++) {
+    if (map[i].indexOf('^') > 0) {
+      guardRow = i;
+      guardCol = map[i].indexOf('^');
+    }
+  }
+
+  return [guardRow, guardCol];
+}
